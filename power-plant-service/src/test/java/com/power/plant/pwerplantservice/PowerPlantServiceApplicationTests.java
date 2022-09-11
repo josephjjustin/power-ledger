5d5055a7e3b40f36fa0de3024c7be206ddeaea99
justinseera <justin.joseph@seera.sa>
2022-09-11 15:31:43 +0400

    power-plant-client power-plant-service initial commit

---
+++ power-plant-service/src/test/java/com/power/plant/pwerplantservice/PowerPlantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.plant.pwerplantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerPlantServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
